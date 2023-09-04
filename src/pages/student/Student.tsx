51578e5fc0d25ed1b6289c168fca37ab7e5e6625
EstebanGomez <gomez.esteban@outlook.fr>
2023-09-04 22:07:51 +0200

    initial commit

---
+++ src/pages/student/Student.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Student() {
+    return (
+        <h1 className="text-3xl font-bold underline text-red-600">
+            STUDENT
+        </h1>
+    );
+}
+
+export default Student;
