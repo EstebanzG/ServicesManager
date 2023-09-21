7dde332979f3b76cdb44d703d0d192ef11ee48f9
EstebanGomez <gomez.esteban@outlook.fr>
2023-09-21 19:32:59 +0200

    [FIX] grossissement du bouton d'ajout

--- src/pages/listes/week/components/WeekList.tsx
+++ src/pages/listes/week/components/WeekList.tsx
@@ -26,7 +26,7 @@ function WeekList(
                <h4 className={"text-blue-600 text-3xl font-bold ml-10 mr-3"}>Weeks</h4>
                {isLoaded ?
                    <button onClick={addWeek}>
-                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-5 h-5 text-blue-600">
+                       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 text-blue-600">
                            <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 11-18 0 9 9 0 0118 0z" />
                        </svg>
                    </button>
