113b50c8a59374147e40e26ba80672d4431c3d36
EstebanGomez <gomez.esteban@outlook.fr>
2023-12-28 17:05:59 +0100

    [ADD] add column for general update

--- src/pages/actions/repartition/StudentRepartitionList.tsx
+++ src/pages/actions/repartition/StudentRepartitionList.tsx
@@ -1,10 +1,5 @@
 import React, {useEffect, useState} from 'react';
-import {
-    Periode,
-    Service,
-    ServiceDistribution,
-    StudentWithServiceDistrib,
-} from "../../../../database.types";
+import {Periode, Service, ServiceDistribution, StudentWithServiceDistrib,} from "../../../../database.types";
 import {supabase} from "../../../common/supabaseClient";
 import Success from "../../../component/messages/Success";
 
@@ -20,11 +15,11 @@ interface props {
     loadStudents: (weekId: string) => void
 }
 
-const MONDAY = 'monday'
-const THUESDAY = 'thuesday'
-const WEDNESDAY = 'wednesday'
-const THURSDAY = 'thursday'
-const FRIDAY = 'friday'
+const MONDAY: string = 'monday'
+const THUESDAY: string = 'thuesday'
+const WEDNESDAY: string = 'wednesday'
+const THURSDAY: string = 'thursday'
+const FRIDAY: string = 'friday'
 
 const daysOfTheWeek = [
     MONDAY,
@@ -34,8 +29,23 @@ const daysOfTheWeek = [
     FRIDAY
 ]
 
+const dayOfTheWeekMap = (day: string) => {
+    switch (day) {
+        case MONDAY:
+            return 'Lundi';
+        case THUESDAY:
+            return 'Mardi';
+        case WEDNESDAY:
+            return 'Mercredi';
+        case THURSDAY:
+            return 'Jeudi';
+        case FRIDAY:
+            return 'Vendredi';
+    }
+}
 
-function StudentRepartitionList({students, periodes, services, currentService, selectedWeek, reload, setReload, loadStudents}: props) {
+function StudentRepartitionList(
+    {students, periodes, services, currentService, selectedWeek, reload, setReload, loadStudents}: props) {
     const [isloaded, setIsloaded] = useState<boolean>(true);
     const [isRegisterSuccess, setIsRegisterSuccess] = useState<boolean>(false);
 
@@ -43,19 +53,41 @@ function StudentRepartitionList({students, periodes, services, currentService, s
         setReload(false)
     }, [setReload, reload]);
 
-    const findServicesNamesByStudent = (student : StudentWithServiceDistrib) => {
+    const findServicesNamesByStudent = (student: StudentWithServiceDistrib): string[] => {
         if (student.service_distribution.length === 0) {
-            return '';
+            return [];
         }
-        let servicesNames = student.service_distribution.map(service_distrib => findServiceNameById(service_distrib.service_id));
-        return servicesNames.join(', ')
+        return student.service_distribution.map(service_distrib => findServiceNameById(service_distrib.service_id));
     }
 
-    const findServiceNameById = (serviceId: number) => {
-        return services.find(service => service.id === serviceId)?.name;
+    const findServiceNameById = (serviceId: number): string => {
+        let serviceName = services.find(service => service.id === serviceId)?.name;
+        if (serviceName !== undefined) {
+            return serviceName;
+        }
+        return '';
     }
 
-    const findValueOfPeriodByServiceAndByStudent = (student : StudentWithServiceDistrib, day: string): number => {
+    const updateAllFieldsValues = (student: StudentWithServiceDistrib, periodeId: number): void => {
+        daysOfTheWeek.forEach(day => {
+            const select = document.querySelector(`#select-${student.id}-${day}`);
+            if (select instanceof Element) {
+                select.setAttribute('value', periodeId.toString());
+                const options = select.querySelectorAll('option');
+                options.forEach(option => {
+                    if (option.value === periodeId.toString()) {
+                        option.setAttribute('selected', 'selected');
+                        updateValueOfPeriodByServiceAndByStudent(student, day, periodeId)
+                    } else {
+                        option.removeAttribute('selected');
+                    }
+                })
+            }
+        })
+        console.log(periodeId)
+    }
+
+    const findValueOfPeriodByServiceAndByStudent = (student: StudentWithServiceDistrib, day: string): number => {
         let studentDistrib = student.service_distribution.find(serviceDistrib => serviceDistrib.service_id === parseInt(currentService));
         if (studentDistrib === undefined) {
             return 0;
@@ -64,12 +96,12 @@ function StudentRepartitionList({students, periodes, services, currentService, s
         return studentDistrib[day + '_periode'] ? studentDistrib[day + '_periode'] : '';
     }
 
-    const updateValueOfPeriodByServiceAndByStudent = (student : StudentWithServiceDistrib, day: string,  value: number) => {
+    const updateValueOfPeriodByServiceAndByStudent = (student: StudentWithServiceDistrib, day: string, value: number) => {
         let serviceDistributionForThisServiceAndWeek = student.service_distribution.filter(studentDistrib =>
             String(studentDistrib.service_id) === currentService && String(studentDistrib.week_id) === selectedWeek
         )
         if (serviceDistributionForThisServiceAndWeek.length === 0) {
-            let serviceDistrib : ServiceDistribution = {
+            let serviceDistrib: ServiceDistribution = {
                 id: -1,
                 friday_periode: null,
                 thuesday_periode: null,
@@ -101,7 +133,7 @@ function StudentRepartitionList({students, periodes, services, currentService, s
         for (let student of students) {
             let newServiceDistribs = student.service_distribution.filter(serviceDistrib => String(serviceDistrib.service_id) === currentService && serviceDistrib.id === -1)
             if (newServiceDistribs.length !== 0) {
-                for(let newServiceDistrib of newServiceDistribs) {
+                for (let newServiceDistrib of newServiceDistribs) {
                     await supabase
                         .from('service_distribution')
                         .insert([{
@@ -129,29 +161,28 @@ function StudentRepartitionList({students, periodes, services, currentService, s
     return (
         <>
             <div className={"w-full flex flex-col items-center p-1"}>
-                {isRegisterSuccess ? <Success /> : ''}
+                {isRegisterSuccess ? <Success/> : ''}
                 {currentService !== '' ? (
-                    <button type={"submit"} onClick={registerRepartitions} className={"bg-blue-500 w-2/12 flex items-center justify-center rounded-full shadow-lg pl-2 pr-2 pb-1 pt-1 mt-5 mb-5 hover:bg-blue-400"}>
+                    <button type={"submit"} onClick={registerRepartitions}
+                            className={"bg-blue-500 w-2/12 flex items-center justify-center rounded-full shadow-lg pl-2 pr-2 pb-1 pt-1 mt-5 mb-5 hover:bg-blue-400"}>
                         {isloaded ? '' : (
-                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" strokeWidth={1.5} stroke="currentColor" className="animate-spin h-5 w-5 mr-3 text-white" viewBox="0 0 24 24">
-                                <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" strokeWidth={1.5} stroke="currentColor"
+                                 className="animate-spin h-5 w-5 mr-3 text-white" viewBox="0 0 24 24">
+                                <path strokeLinecap="round" strokeLinejoin="round"
+                                      d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99"/>
                             </svg>
                         )}
                         <h4 className={"text-white font-bold text-xl"}>Enregistrer</h4>
                     </button>
-                ) : '' }
-                <table className={"table-auto w-10/12 mb-7"}>
+                ) : ''}
+                <table className={"w-10/12 mb-7"}>
                     <thead>
-                        <tr className={"border-b-2 border-blue-500"}>
-                            <th className={"w-3/12"}>Nom - Prénom</th>
-                            <th className={"w-2/12"}>Classe</th>
-                            <th className={"w-2/12"}>Service</th>
-                            <th className={"w-1/12"}>Lundi</th>
-                            <th className={"w-1/12"}>Mardi</th>
-                            <th className={"w-1/12"}>Mercredi</th>
-                            <th className={"w-1/12"}>Jeudi</th>
-                            <th className={"w-1/12"}>Vendredi</th>
-                        </tr>
+                    <tr className={"border-b-2 border-blue-500 text-left"}>
+                        <th className={'p-2'}>Nom - Prénom</th>
+                        <th className={'p-2'}>Classe</th>
+                        <th className={'p-2'}>Service</th>
+                        <th className={'p-2'}>Répartition</th>
+                    </tr>
                     </thead>
                     <tbody>
                     {
@@ -159,23 +190,53 @@ function StudentRepartitionList({students, periodes, services, currentService, s
                             <tr key={student.id} className={"border-b-2 border-blue-100 hover:bg-blue-100"}>
                                 <td className={""}>{student.lastname + "-" + student.firstname}</td>
                                 <td className={""}>{student.classe?.name}</td>
-                                <td className={""}>{findServicesNamesByStudent(student) !== '' ? findServicesNamesByStudent(student) : ''}</td>
-                                {daysOfTheWeek.map(day => (
-                                    <td key={day}>
+                                <td className={"w-fit"}>
+                                    <ul className={"flex flex-col list-disc"}>
+                                        {findServicesNamesByStudent(student).map((serviceName) => (
+                                                <li key={serviceName}>{serviceName}</li>
+                                            )
+                                        )}
+                                    </ul>
+                                </td>
+                                <td className={"flex flex-col"}>
+                                    {daysOfTheWeek.map(day => (
+                                        <div key={day} className={"flex justify-between"}>
+                                            <label htmlFor={`select-${student.id}-${day}`}>{dayOfTheWeekMap(day)}</label>
+                                            {currentService !== '' && !reload ? (
+                                                <select
+                                                    className={`border border-b-2 rounded`}
+                                                    onChange={e => updateValueOfPeriodByServiceAndByStudent(student, day, parseInt(e.target.value))}
+                                                    defaultValue={findValueOfPeriodByServiceAndByStudent(student, day)}
+                                                    id={`select-${student.id}-${day}`}
+                                                >
+                                                    <option></option>
+                                                    {periodes.map((periode) => (
+                                                        <option key={periode.id}
+                                                                value={periode.id}>{periode.name}</option>
+                                                    ))}
+                                                </select>
+                                            ) : ''}
+                                        </div>
+                                    ))}
+                                    <div>
                                         {currentService !== '' && !reload ? (
-                                            <select
-                                                className={`border border-b-2 rounded`}
-                                                onChange={e => updateValueOfPeriodByServiceAndByStudent(student, day, parseInt(e.target.value))}
-                                                defaultValue={findValueOfPeriodByServiceAndByStudent(student, day)}
-                                            >
-                                                <option></option>
-                                                {periodes.map((periode) => (
-                                                    <option key={periode.id} value={periode.id}>{periode.name}</option>
-                                                ))}
-                                            </select>
+                                            <div className={"flex justify-between"}>
+                                                <label htmlFor={`select-all`}>Tous les jours</label>
+                                                <select
+                                                    className={`border border-b-2 rounded`}
+                                                    onChange={e => updateAllFieldsValues(student, parseInt(e.target.value))}
+                                                    id={'select-all'}
+                                                >
+                                                    <option></option>
+                                                    {periodes.map((periode) => (
+                                                        <option key={periode.id} value={periode.id}>{periode.name}</option>
+                                                    ))}
+                                                </select>
+                                            </div>
                                         ) : ''}
-                                    </td>
-                                ))}
+                                    </div>
+                                </td>
+
                             </tr>
                         ))
                     }
